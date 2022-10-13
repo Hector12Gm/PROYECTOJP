515c03ef5e23fb091186860f1b1ddcdd38aa9c58
Maximiliano522 <josemaximilianoep@hotmail.com>
2022-10-13 14:09:42 -0500

    Dividimos en funciones evadir obstaculos y seguir trayectoria

--- trajectory.py
+++ trajectory.py
@@ -1,10 +1,46 @@
 
+from queue import Empty
 from matplotlib.pyplot import sci
 import numpy as  np
+import sys
 import scipy.interpolate as spi
 import math as m
+import sim
+import matplotlib.pyplot as plt
+import time
 
 
+
+
+xt = []
+yt = []
+
+Kv = 1
+Kh = 2.5
+
+
+t=time.time
+
+hd = 0
+r = 0.5*0.195
+L = 0.311
+errp = 10
+
+xarr = np.array([0,0.5,1,2.5,3, 3, 3,3,2.5,0.5,0, 0,0, 0,0])
+yarr = np.array([0, 0,0, 0,0,0.5,2.5,3, 3, 3,3,2.5,1,0.5,0])
+
+plt.scatter(xarr,yarr)
+
+
+def angdiff(t1, t2):
+    """
+    Compute the angle difference, t2-t1, restricting the result to the [-pi,pi] range
+    """
+    # The angle magnitude comes from the dot product of two vectors
+    angmag = m.acos(m.cos(t1)*m.cos(t2)+m.sin(t1)*m.sin(t2))
+    # The direction of rotation comes from the sign of the cross product of two vectors
+    angdir = m.cos(t1)*m.sin(t2)-m.sin(t1)*m.cos(t2)
+    return m.copysign(angmag, angdir)
 
 def interp(tiempo,x,y):
     ttime =  70
@@ -19,12 +55,31 @@ def interp(tiempo,x,y):
 
     return {'x':xnew,'y':ynew}
 
-def angdiff(t1, t2):
-    """
-    Compute the angle difference, t2-t1, restricting the result to the [-pi,pi] range
-    """
-    # The angle magnitude comes from the dot product of two vectors
-    angmag = m.acos(m.cos(t1)*m.cos(t2)+m.sin(t1)*m.sin(t2))
-    # The direction of rotation comes from the sign of the cross product of two vectors
-    angdir = m.cos(t1)*m.sin(t2)-m.sin(t1)*m.cos(t2)
-    return m.copysign(angmag, angdir)
+def Trajectory(client_id, robot,ts,t):
+
+    
+    tau = ts  -  t
+    ret, carpos = sim.simxGetObjectPosition(client_id, robot, -1, sim.simx_opmode_blocking)
+    ret, carrot = sim.simxGetObjectOrientation(client_id, robot, -1, sim.simx_opmode_blocking)
+
+    data = interp(tau,xarr,yarr)
+    xd = data['x']
+    yd = data['y']
+
+    errp = m.sqrt((xd-carpos[0])**2 + (yd-carpos[1])**2)
+    angd = m.atan2(yd-carpos[1], xd-carpos[0])
+    errh = angdiff(carrot[2], angd)
+    print('Distance to goal: {}   Heading error: {}'.format(errp, errh))
+
+    v = Kv*errp
+    omega = Kh*errh
+
+    ul = v/r - L*omega/(2*r)
+    ur = v/r + L*omega/(2*r)
+
+    xt.append(carpos[0])
+    yt.append(carpos[1])
+    
+        #time.sleep(0.1)
+    return(ul,ur,xt,yt)
+    
